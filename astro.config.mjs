b52f430dddb6d6a6e54f15a912d2c34b4538f7f5
45yposft <crsoedjh@gmail.com>
2025-10-15 14:02:57 +0100

    Creado proyecto
    Preparado sistema de carpetas, creadas las plantillas necesarias, preparadas las imágenes, ajustado opciones en fichero .mjs y empezar a tocar estilos en .css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  //site: "urlcanonica", 
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport'
+  }
+
+});
